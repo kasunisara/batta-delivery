1ed4a67720945bf5b96660d18f49832750d7e3a2
Kasuni sarangi silva <sarangik909@gmail.com>
2023-10-24 16:29:01 +0530

    next-upload

---
+++ wp-content/themes/batta-delivery/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <title>Batta Delivery</title>
+</head>
+<body>
+   <p>This is a test paragraph</p> 
+</body>
+</html>
