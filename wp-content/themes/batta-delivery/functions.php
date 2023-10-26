e1ea847796c9cff38cda995e68b130a010c683c2
Kasuni sarangi silva <sarangik909@gmail.com>
2023-10-26 10:22:48 +0530

    upload

--- wp-content/themes/batta-delivery/functions.php
+++ wp-content/themes/batta-delivery/functions.php
@@ -11,7 +11,7 @@
  function batta_delivery_scripts(){
      wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array('jquery'), '5.3.2', true);
      wp_enqueue_style('bootstrap-css',get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '5.3.2', 'all' );
-     wp_enqueue_style('batta-delivery-style',get_stylesheet_uri(), array(), '1.0', 'all' );
+     wp_enqueue_style('batta-delivery-style',get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css'), 'all' );
  }
  add_action('wp_enqueue_scripts', 'batta_delivery_scripts' );
 
